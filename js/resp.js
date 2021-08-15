18deaf5741b960114a28e8d60915069e4b102229
Umar <umarshaikhshaikh7@gmail.com>
2021-08-15 15:15:46 +0530

    firts commit

---
+++ js/resp.js
@@ -0,0 +1,7 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.nav-list')
+burger.addEventListener('click',()=>{
+      navlist.classList.toggle('v-class')
+      navbar.classList.toggle('h-nav')
+})
