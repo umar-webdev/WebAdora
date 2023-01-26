a4d5904dbf1f4419b2740dac3d0783756c38d30b
Umar Shaikh <63943753+umarkali@users.noreply.github.com>
2023-01-26 22:14:31 +0530

    Delete js directory

--- js/resp.js
+++
@@ -1,7 +0,0 @@
-burger = document.querySelector('.burger')
-navbar = document.querySelector('.navbar')
-navlist = document.querySelector('.nav-list')
-burger.addEventListener('click',()=>{
-      navlist.classList.toggle('v-class')
-      navbar.classList.toggle('h-nav')
-})
